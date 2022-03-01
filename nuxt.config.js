1f1038421d628ec16795d27328c4fdb3a18c90c2
sebastian <sebastian@phineal.com>
2022-02-28 21:04:26 -0300

    update name

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'front-ges',
+    title: 'front-gis',
     htmlAttrs: {
       lang: 'en'
     },
